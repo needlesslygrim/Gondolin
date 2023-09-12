2657f604745f42f8f7fb110daf9a8b91ebee2098
Erick Howard <needlesslygrim@proton.me>
2023-09-12 10:42:48 +0800

    Added the `remove` API endpoint
    It's now possible to remove logins via the API. Only one can be removed
    at a time, due to logins not having an ID, and only having an index in
    the array. However, it works for now so it's probably alright.
    
    - Also added some comments about what might need to be done to the other
      functions
    - Added a TODO list so I don't forget what I'm doing
    - Fixed up some other things which I've definitely forgotten about :(
    
    Signed-off-by: Erick Howard <needlesslygrim@proton.me>

---
+++ TODO.md
@@ -0,0 +1,9 @@
+- [ ] Use URL query parameters instead of passing the query in the body of the GET request
+- [ ] Allow removing more than one login at a time
+- [ ] Web interface
+- [ ] API
+  - [ ] Initialise a database via the API
+  - [X] Add logins via the API
+  - [X] Query logins via the API
+  - [ ] Remove logins via the API **IN PROGRESS**
+    - [X] Remove a single login
--- src/net.rs
+++ src/net.rs
@@ -1,4 +1,4 @@
-use std::io::{BufReader, Read};
+use std::io::{BufReader, Cursor, Read};
 
 use color_eyre::eyre::{Result, WrapErr};
 use tiny_http::{Header, Request, Response, StatusCode};
@@ -26,13 +26,14 @@ pub fn serve(db: &mut Database) -> Result<()> {
                 &include_bytes!("index.js")[..],
                 "text/javascript; charset=utf8",
             ),
-            (M::Get, "/index.css") => serve_bytes(
-                request,
-                &include_bytes!("../dist/output.css")[..],
-                "text/css; charset=utf8",
-            ),
+            // (M::Get, "/index.css") => serve_bytes(
+            //     request,
+            //     &include_bytes!("../dist/output.css")[..],
+            //     "text/css; charset=utf8",
+            // ),
             (M::Get, "/api/query") => serve_query(request, db),
             (M::Post, "/api/new") => add_new(request, db),
+            (M::Delete, "/api/remove") => remove_login(request, db),
             _ => serve_404(request),
         }
     }
@@ -49,17 +50,21 @@ fn serve_bytes(request: Request, content: &[u8], content_type: &str) {
     }
 }
 
+// We should probably allow multiple mime types to be put in the response, by looking at the `Accept` header.
+// However, for now there's probably not much point since we're the only ones consuming this API. Therefore
+// we just ignore all headers, and send back `application/json`.
+// TODO: Maybe look at checking the header to at least see if JSON was requested, and if not return 415 with `Accept-Post` set.
 fn serve_query(mut request: Request, db: &Database) {
-    let body_length = request.body_length();
+    let body_length = request.body_length().map(|size| size / 8).unwrap_or(0);
     let reader = request.as_reader();
     let mut buf_reader = BufReader::new(reader);
-    let mut content = String::with_capacity(body_length.map(|len| len / 8).unwrap_or(0));
+    let mut content = String::with_capacity(body_length);
 
     if let Err(e) = buf_reader.read_to_string(&mut content) {
         eprintln!("[-] WARN: Failed to read content body of a request: {e}");
-        if let Err(e) =
-            request.respond(Response::from_string("500").with_status_code(StatusCode(500)))
-        {
+        if let Err(e) = request.respond(
+            Response::from_string(StatusCode(500).default_reason_phrase()).with_status_code(500),
+        ) {
             eprintln!("[|] WARN: Failed to respond to a request: {:#?}", e);
         }
         return;
@@ -83,7 +88,9 @@ fn serve_query(mut request: Request, db: &Database) {
     let body = unsafe { body.unwrap_unchecked() };
     let header = Header::from_bytes("Content-Type", "application/json")
         .expect("Don't put rubbish in here please");
-    let response = Response::from_string(body).with_header(header);
+    let response = Response::from_string(body)
+        .with_header(header)
+        .with_status_code(200);
 
     if let Err(e) = request.respond(response) {
         eprintln!("[|] WARN: Failed to respond to a request: {:#?}", e);
@@ -91,7 +98,7 @@ fn serve_query(mut request: Request, db: &Database) {
 }
 
 fn add_new(mut request: Request, db: &mut Database) {
-    let body_length = request.body_length().map(|length| length / 8).unwrap_or(0);
+    let body_length = request.body_length().map(|size| size / 8).unwrap_or(0);
     let mut buf: Vec<u8> = Vec::with_capacity(body_length);
     let maybe_content_type = request
         .headers()
@@ -153,16 +160,99 @@ fn add_new(mut request: Request, db: &mut Database) {
 
     db.logins.append(&mut logins);
     if let Err(e) = request.respond(
-        Response::from_string(StatusCode(200).default_reason_phrase())
-            .with_status_code(StatusCode(200)),
+        Response::from_string(StatusCode(201).default_reason_phrase()).with_status_code(201),
     ) {
         eprintln!("[|] WARN: Failed to respond to a request: {:#?}", e);
     };
 }
 
 #[inline(always)]
-fn make_415() -> Response<std::io::Cursor<std::vec::Vec<u8>>> {
-    Response::from_string(StatusCode(415).default_reason_phrase()).with_status_code(StatusCode(415))
+fn make_415() -> Response<Cursor<Vec<u8>>> {
+    Response::from_string(StatusCode(415).default_reason_phrase()).with_status_code(415)
+}
+
+// Not idempotent as it should be according to https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE.
+// TODO: Looking into this at a later time might be a good idea, but for now it should be ok.
+fn remove_login(mut request: Request, db: &mut Database) {
+    let body_length = request.body_length().map(|size| size / 8).unwrap_or(0);
+    let mut buf: Vec<u8> = Vec::with_capacity(body_length);
+    let maybe_content_type = request
+        .headers()
+        .iter()
+        .find(|header| header.field.as_str() == "Content-Type");
+    let content_type_header = if maybe_content_type.is_none() {
+        eprintln!("[|] WARN: A request was made to `/api/remove` without a `Content-Type` header");
+        if let Err(e) = request.respond(make_415()) {
+            eprintln!("[|] WARN: Failed to respond to a request: {:#?}", e);
+            return;
+        }
+        return;
+    } else {
+        // Should be fine :^)
+        unsafe { maybe_content_type.unwrap_unchecked() }
+    };
+
+    if content_type_header.value != "application/json" {
+        eprintln!("[|] WARN: A request was made to `/api/remove` without a valid `Content-Type` of `application/json`");
+        if let Err(e) = request.respond(make_415()) {
+            eprintln!("[|] WARN: Failed to respond to a request: {:#?}", e);
+            return;
+        }
+        return;
+    }
+
+    if let Err(e) = request.as_reader().read_to_end(&mut buf) {
+        eprintln!("[|] WARN: Could not read the body of the request: {:#?}", e);
+        if let Err(e) = request.respond(make_415()) {
+            eprintln!("[|] WARN: Failed to respond to a request: {:#?}", e);
+            return;
+        }
+        return;
+    }
+
+    let content = match String::from_utf8(buf) {
+        Ok(content) => content,
+        Err(e) => {
+            eprintln!(
+                "[|] WARN: The body of a request could not be interpreted as UTF-8: {:#?}",
+                e
+            );
+            return;
+        }
+    };
+
+    // FIXME: Add back functionanlity to remove more than one login at a time.
+    // This will probably require adding an ID field to `Login` :[.
+    let index: Result<usize, _> = serde_json::de::from_str(&content);
+    let index = if let Err(e) = index {
+        eprintln!("[-] WARN: Failed to parse login from request: {e}");
+        if let Err(e) = request.respond(make_415()) {
+            eprintln!("[|] WARN: Failed to respond to a request: {:#?}", e);
+            return;
+        }
+        return;
+    } else {
+        // Should be fine :).
+        unsafe { index.unwrap_unchecked() }
+    };
+
+    if index >= db.logins.len() {
+        let response =
+            Response::from_string(StatusCode(400).default_reason_phrase()).with_status_code(400);
+        if let Err(e) = request.respond(response) {
+            eprintln!("[|] WARN: Failed to respond to a request: {:#?}", e);
+            return;
+        }
+        return;
+    }
+
+    db.logins.swap_remove(index);
+
+    if let Err(e) = request.respond(
+        Response::from_string(StatusCode(204).default_reason_phrase()).with_status_code(204),
+    ) {
+        eprintln!("[|] WARN: Failed to respond to a request: {:#?}", e);
+    };
 }
 
 fn serve_404(request: Request) {
